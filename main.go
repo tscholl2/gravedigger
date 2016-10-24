3f765507f93f689297eed3467b89e1efe64cc174
Travis <tscholl2@uw.edu>
2016-10-24 09:08:26 -0700

    add guru referencing as an option

--- main.go
+++ main.go
@@ -5,18 +5,23 @@ for all code in a given directory.
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
+
+	"golang.org/x/tools/oracle/serial"
 )
 
 var (
-	dir = "."
+	dir     = "."
+	useGuru bool
 )
 
 type declaration struct {
@@ -25,6 +30,7 @@ type declaration struct {
 }
 
 func main() {
+	flag.BoolVar(&useGuru, "guru", false, "use guru for name referencing (SLOW)")
 	flag.CommandLine.Usage = func() {
 		fmt.Println(`gravedigger: looks for unused code in a directory. This differs
 from other packages in that it takes a directory and lists things that are unused
@@ -32,7 +38,9 @@ any where in that directory, including exported things in subpackages/subdirecto
 Example: 'gravedigger'
 Example: 'gravedigger .'
 Example: 'gravedigger test/'
+Options: 
 `)
+		flag.PrintDefaults()
 	}
 	flag.Parse()
 	if flag.NArg() > 0 {
@@ -65,14 +73,18 @@ Example: 'gravedigger test/'
 	mark(fileSet, packages, declarations)
 
 	for _, dec := range declarations {
-		fmt.Printf("* %s = %s\n", dec.node.Name, dec.pos)
+		fmt.Printf("* %s = %s\n", dec.node.Name, dec.pos.String())
 	}
 
 	// Step 2: go through and unmark all used functions
 
 	fmt.Printf("\n---------step 2-------------(mark all used declarations)\n\n")
 
-	unmark(fileSet, packages, declarations)
+	if useGuru {
+		unmarkGuru(fileSet, packages, &declarations)
+	} else {
+		unmarkFast(fileSet, packages, &declarations)
+	}
 
 	// Step 3: return a list of unused functions
 
@@ -111,22 +123,42 @@ func mark(fileSet *token.FileSet, packages []*ast.Package, declarations map[stri
 				ast.Inspect(n, func(n ast.Node) bool {
 					switch node := n.(type) {
 					case *ast.FuncDecl:
-						if node.Name.Name == "init" || node.Name.Name == "_" || (p.Name == "main" && node.Name.Name == "main") {
+						// TODO: check signature instead of name
+						if node.Name.Name == "init" || node.Name.Name == "MarshalJSON" || node.Name.Name == "Scan" || node.Name.Name == "Value" || (p.Name == "main" && node.Name.Name == "main") {
 							return true
 						}
 						dec := declaration{fileSet.Position(node.Name.Pos()), node.Name}
-						declarations[dec.node.Name] = dec
+						filename, _ := filepath.Abs(dec.pos.Filename)
+						declarations[fmt.Sprintf("%s:%d:%d", filename, dec.pos.Line, dec.pos.Column)] = dec
 					case *ast.GenDecl:
 						for _, spec := range node.Specs {
 							switch s := spec.(type) {
 							case *ast.ValueSpec:
 								for _, n := range s.Names {
 									dec := declaration{fileSet.Position(n.Pos()), n}
-									declarations[dec.node.Name] = dec
+									filename, _ := filepath.Abs(dec.pos.Filename)
+									declarations[fmt.Sprintf("%s:%d:%d", filename, dec.pos.Line, dec.pos.Column)] = dec
 								}
 							case *ast.TypeSpec:
 								dec := declaration{fileSet.Position(s.Name.Pos()), s.Name}
-								declarations[dec.node.Name] = dec
+								filename, _ := filepath.Abs(dec.pos.Filename)
+								declarations[fmt.Sprintf("%s:%d:%d", filename, dec.pos.Line, dec.pos.Column)] = dec
+								/*
+									ast.Inspect(s.Type, func(n ast.Node) bool {
+										node, ok := n.(*ast.StructType)
+										if !ok {
+											return true
+										}
+										for _, node2 := range node.Fields.List {
+											for _, node3 := range node2.Names {
+												dec := declaration{fileSet.Position(node3.Pos()), node3}
+												filename, _ := filepath.Abs(dec.pos.Filename)
+												declarations[fmt.Sprintf("%s:%d:%d", filename, dec.pos.Line, dec.pos.Column)] = dec
+											}
+										}
+										return true
+									})
+								*/
 							}
 						}
 					}
@@ -137,7 +169,11 @@ func mark(fileSet *token.FileSet, packages []*ast.Package, declarations map[stri
 	}
 }
 
-func unmark(fileSet *token.FileSet, packages []*ast.Package, declarations map[string]declaration) {
+func unmarkFast(fileSet *token.FileSet, packages []*ast.Package, declarations *map[string]declaration) {
+	simpleDeclarations := make(map[string]declaration)
+	for _, dec := range *declarations {
+		simpleDeclarations[dec.node.Name] = dec
+	}
 	for _, p := range packages {
 		for _, f := range p.Files {
 			ast.Inspect(f, func(n ast.Node) bool {
@@ -145,12 +181,83 @@ func unmark(fileSet *token.FileSet, packages []*ast.Package, declarations map[st
 				if !ok {
 					return true
 				}
-				dec, _ := declarations[node.Name]
+				dec, ok := simpleDeclarations[node.Name]
+				if !ok {
+					return true
+				}
 				curpos := fileSet.Position(node.Pos())
 				if dec.pos.Filename == curpos.Filename && dec.pos.Line == curpos.Line && dec.pos.Column == curpos.Column {
 					return true
 				}
-				delete(declarations, node.Name)
+				fmt.Println(node.Name)
+				fmt.Println("used in: ", curpos.String())
+				fmt.Println("defd in: ", dec.pos.String())
+				delete(simpleDeclarations, node.Name)
+				return true
+			})
+		}
+	}
+	*declarations = make(map[string]declaration)
+	for _, dec := range simpleDeclarations {
+		(*declarations)[dec.pos.String()] = dec
+	}
+}
+
+func unmarkGuru(fileSet *token.FileSet, packages []*ast.Package, declarations *map[string]declaration) {
+	names := make(map[string]int)
+	for _, dec := range *declarations {
+		i, _ := names[dec.node.Name]
+		names[dec.node.Name] = i + 1
+	}
+	updateNames := func(name string) {
+		i, ok := names[name]
+		if ok && i > 1 {
+			names[name] = i - 1
+		} else {
+			delete(names, name)
+		}
+	}
+	for _, p := range packages {
+		for _, f := range p.Files {
+			ast.Inspect(f, func(n ast.Node) bool {
+				node, ok := n.(*ast.Ident)
+				if !ok {
+					return true
+				}
+				_, ok = names[node.Name]
+				if !ok {
+					return true
+				}
+				position := fileSet.Position(node.Pos())
+				currentFile, _ := filepath.Abs(position.Filename)
+				out, err := exec.Command("guru", "-json", "definition", fmt.Sprintf("%s:#%d", currentFile, position.Offset)).Output()
+				if err != nil && err.Error() != "exit status 1" {
+					panic(err)
+				}
+				if len(out) == 0 {
+					return true
+				}
+				var def serial.Definition
+				if err := json.Unmarshal(out, &def); err != nil {
+					panic(err)
+				}
+				if def.ObjPos == "" {
+					return true
+				}
+				currentPosition := fmt.Sprintf("%s:%d:%d", currentFile, position.Line, position.Column)
+				defPosition := def.ObjPos
+				if currentPosition == defPosition {
+					return true
+				}
+				dec, ok := (*declarations)[defPosition]
+				if !ok {
+					return true
+				}
+				fmt.Println(node.Name)
+				fmt.Println("used in: ", position.String())
+				fmt.Println("defd in: ", dec.pos.String())
+				delete(*declarations, defPosition)
+				updateNames(dec.node.Name)
 				return true
 			})
 		}
