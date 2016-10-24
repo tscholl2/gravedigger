9ef7e47a8bc7c1fc8e34daf3010f42aafa38e54a
Travis Scholl <tscholl2@uw.edu>
2016-10-23 17:41:57 -0700

    trying simplest possible version

--- main.go
+++ main.go
@@ -5,19 +5,14 @@ for all code in a given directory.
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
 	"os"
-	"os/exec"
 	"path/filepath"
-	"strconv"
 	"strings"
-
-	"golang.org/x/tools/cmd/guru/serial"
 )
 
 var (
@@ -26,7 +21,7 @@ var (
 
 type declaration struct {
 	pos  token.Position
-	name string
+	node *ast.Ident
 }
 
 func (d declaration) String() string {
@@ -37,9 +32,19 @@ func (d declaration) String() string {
 	return fmt.Sprintf("%s:%d:%d", fn, d.pos.Line, d.pos.Column)
 }
 
+func shortenPath(path string) string {
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	s, err := filepath.Rel(cwd, path)
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
+
 func main() {
-	var fast bool
-	flag.BoolVar(&fast, "fast", false, "gives a very quick but almost reasonable analysis")
 	flag.CommandLine.Usage = func() {
 		fmt.Println(`gravedigger: looks for unused code in a directory. This differs
 from other packages in that it takes a directory and lists things that are unused
@@ -47,9 +52,7 @@ any where in that directory, including exported things in subpackages/subdirecto
 Example: 'gravedigger'
 Example: 'gravedigger .'
 Example: 'gravedigger test/'
-Options:
 `)
-		flag.CommandLine.PrintDefaults()
 	}
 	flag.Parse()
 	if flag.NArg() > 0 {
@@ -69,6 +72,12 @@ Options:
 
 	parse(fileSet, packages, declarations)
 
+	for _, p := range packages {
+		for _, f := range p.Files {
+			fmt.Println(fileSet.Position(f.Pos()).Filename)
+		}
+	}
+
 	// Step 1: go through and mark all declarations
 
 	fmt.Printf("\n---------step 1-------------(mark all declarations)\n\n")
@@ -76,39 +85,23 @@ Options:
 	mark(fileSet, packages, declarations)
 
 	for _, dec := range declarations {
-		fmt.Printf("* %s = %s\n", dec.name, dec.pos)
+		fmt.Printf("* %s = %s\n", dec.node.Name, dec.pos)
 	}
 
 	// Step 2: go through and unmark all used functions
 
 	fmt.Printf("\n---------step 2-------------(mark all used declarations)\n\n")
 
-	if fast {
-		unmarkFast(fileSet, packages, declarations)
-	} else {
-		unmarkGuru(fileSet, packages, declarations)
-	}
+	unmark(fileSet, packages, declarations)
 
 	// Step 3: return a list of unused functions
 
 	fmt.Printf("\n---------step 3-------------(list all unused declarations)\n\n")
 
 	for _, dec := range declarations {
-		fmt.Printf("* %s = %s\n", dec.name, dec.pos)
+		fmt.Printf("* %s = %s\n", dec.node.Name, dec.pos)
 	}
 
-}
-
-func shortenPath(path string) string {
-	cwd, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-	s, err := filepath.Rel(cwd, path)
-	if err != nil {
-		panic(err)
-	}
-	return s
 }
 
 func parse(fileSet *token.FileSet, packages []*ast.Package, declarations map[string]declaration) {
@@ -129,12 +122,6 @@ func parse(fileSet *token.FileSet, packages []*ast.Package, declarations map[str
 	}); err != nil {
 		panic(err)
 	}
-
-	for _, p := range packages {
-		for _, f := range p.Files {
-			fmt.Println(fileSet.Position(f.Pos()).Filename)
-		}
-	}
 }
 
 func mark(fileSet *token.FileSet, packages []*ast.Package, declarations map[string]declaration) {
@@ -147,36 +134,19 @@ func mark(fileSet *token.FileSet, packages []*ast.Package, declarations map[stri
 						if node.Name.Name == "init" || node.Name.Name == "_" || (p.Name == "main" && node.Name.Name == "main") {
 							return true
 						}
-						dec := declaration{fileSet.Position(node.Name.Pos()), node.Name.Name}
-						declarations[dec.String()] = dec
+						dec := declaration{fileSet.Position(node.Name.Pos()), node.Name}
+						declarations[dec.node.Name] = dec
 					case *ast.GenDecl:
-						// var, const, types
 						for _, spec := range node.Specs {
 							switch s := spec.(type) {
 							case *ast.ValueSpec:
-								// constants and variables
 								for _, n := range s.Names {
-									dec := declaration{fileSet.Position(n.Pos()), n.Name}
-									declarations[dec.String()] = dec
+									dec := declaration{fileSet.Position(n.Pos()), n}
+									declarations[dec.node.Name] = dec
 								}
 							case *ast.TypeSpec:
-								// type definitions
-								dec := declaration{fileSet.Position(s.Name.Pos()), s.Name.Name}
-								declarations[dec.String()] = dec
-								// add struct fields?
-								ast.Inspect(s.Type, func(n ast.Node) bool {
-									node, ok := n.(*ast.StructType)
-									if !ok {
-										return true
-									}
-									for _, node2 := range node.Fields.List {
-										for _, node3 := range node2.Names {
-											dec := declaration{fileSet.Position(node3.Pos()), node3.Name}
-											declarations[dec.String()] = dec
-										}
-									}
-									return true
-								})
+								dec := declaration{fileSet.Position(s.Name.Pos()), s.Name}
+								declarations[dec.node.Name] = dec
 							}
 						}
 					}
@@ -187,20 +157,7 @@ func mark(fileSet *token.FileSet, packages []*ast.Package, declarations map[stri
 	}
 }
 
-func unmarkFast(fileSet *token.FileSet, packages []*ast.Package, declarations map[string]declaration) {
-	names := make(map[string]string)
-	toRemove := []string{}
-	for f, dec := range declarations {
-		_, ok := names[dec.name]
-		if ok {
-			toRemove = append(toRemove, f)
-		} else {
-			names[dec.name] = f
-		}
-	}
-	for _, f := range toRemove {
-		delete(declarations, f)
-	}
+func unmark(fileSet *token.FileSet, packages []*ast.Package, declarations map[string]declaration) {
 	for _, p := range packages {
 		for _, f := range p.Files {
 			ast.Inspect(f, func(n ast.Node) bool {
@@ -208,65 +165,12 @@ func unmarkFast(fileSet *token.FileSet, packages []*ast.Package, declarations ma
 				if !ok {
 					return true
 				}
-				f, ok := names[node.Name]
-				if !ok {
-					return true
-				}
-				dec, ok := declarations[f]
-				if !ok {
-					return true
-				}
+				dec, _ := declarations[node.Name]
 				curpos := fileSet.Position(node.Pos())
 				if dec.pos.Filename == curpos.Filename && dec.pos.Line == curpos.Line && dec.pos.Column == curpos.Column {
 					return true
 				}
-				delete(declarations, f)
-				return true
-			})
-		}
-	}
-}
-
-func unmarkGuru(fileSet *token.FileSet, packages []*ast.Package, declarations map[string]declaration) {
-	for _, p := range packages {
-		for _, f := range p.Files {
-			ast.Inspect(f, func(n ast.Node) bool {
-				node, ok := n.(*ast.Ident)
-				if !ok {
-					return true
-				}
-				currentPos := fileSet.Position(node.Pos())
-				currentFile, _ := filepath.Abs(currentPos.Filename)
-				// fmt.Println("guru -json definition " + fmt.Sprintf("%s:#%d", fn, pos.Offset))
-				out, err := exec.Command("guru", "-json", "definition", fmt.Sprintf("%s:#%d", currentFile, currentPos.Offset)).Output()
-				if err != nil && err.Error() != "exit status 1" {
-					panic(err)
-				}
-				var def serial.Definition
-				json.Unmarshal(out, &def)
-				if def.ObjPos == "" {
-					return true
-				}
-				// fmt.Println("found definition of ", node.Name)
-				// fmt.Println(def)
-				arr := strings.Split(def.ObjPos, ":")
-				defLine, _ := strconv.Atoi(arr[1])
-				defColumn, _ := strconv.Atoi(arr[2])
-				defFile := arr[0] //path.Join(build.Default.GOPATH, "src", w.p.dir, w.f.Name.Name+".go")
-				// fmt.Println("found node: ", node.Name)
-				// fmt.Println("node at: ", currentFile, currentPos.Line, currentPos.Column)
-				// fmt.Println("defn at: ", defFile, defLine, defColumn)
-				if currentFile == defFile && currentPos.Line == defLine && currentPos.Column == defColumn {
-					return true
-				}
-				_, ok = declarations[fmt.Sprintf("%s:%d:%d", defFile, defLine, defColumn)]
-				if !ok {
-					return true
-				}
-				fmt.Println(node.Name)
-				fmt.Println("used in: ", shortenPath(currentFile), currentPos.Line, currentPos.Column)
-				fmt.Println("defd in: ", shortenPath(defFile), defLine, defColumn)
-				delete(declarations, fmt.Sprintf("%s:%d:%d", defFile, defLine, defColumn))
+				delete(declarations, node.Name)
 				return true
 			})
 		}
